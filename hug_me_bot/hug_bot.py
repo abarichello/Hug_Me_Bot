c973916a41c4c9aab9348c7af168140b17b691e4
aBARICHELLO <arturbarichello@hotmail.com>
2017-07-18 16:50:48 -0300

    Typed username formatting

--- hug_me_bot/hug_bot.py
+++ hug_me_bot/hug_bot.py
@@ -7,8 +7,15 @@ InlineQueryHandler)
 import logging, re, random
 
 HELP_STRING = ("""
-useful helpful text
-""")
+Hello! I am an inline bot!
+To use me type @Hug_Me_Bot in any chat,
+after that type the username of the person that needs a hug
+and finally select the desired emote.
+Free hugs guaranteed.
+
+Need help? Contact @aBARICHELLO
+Star me on github! https://github.com/aBARICHELLO/Hug_Me_Bot
+""").strip('\n')
 
 def start(bot, update):
     update.message.reply_text("Hi!")
@@ -17,9 +24,9 @@ def help(bot, update):
     update.message.reply_text(HELP_STRING)
 
 def escape_markdown(text):
-    escape_chars = '\*_`\['
-    Ntext = re.sub(r'([%s])' % escape_chars, r'\\\1', text)
-    return Ntext
+    escape_chars = ' \*_`\['
+    text = re.sub(r'([%s])' % escape_chars, r'\\\1', text)
+    return text
 
 def phrase_generator():
     number = random.randint(0, PHRASES_LENGTH)
@@ -31,15 +38,14 @@ def inlinequery(bot, update):
     results = list()
 
     sender_username = update.inline_query.from_user.username
+    if sender_username == None:
+        sender_username = update.inline_query.from_user.first_name
     pre_phrase = "@{} sends ".format(sender_username)
     phrase = phrase_generator()
     typed_username = escape_markdown(query)
     hug_emote = '(>^-^)>'
 
-    if '@' not in typed_username:
-        typed_username = "@{}".format(typed_username)
-
-    typed_name = InputTextMessageContent(str(query))
+    typed_username = formatTypedUsername(typed_username)
 
     results.append(InlineQueryResultArticle(id=1, 
         title="{} I will hug {}".format(hug_emote, str(query)), 
@@ -56,6 +62,13 @@ def inlinequery(bot, update):
 
     update.inline_query.answer(results)
 
+def formatTypedUsername(typed_username):
+    if len(typed_username) > 4 and '@' not in typed_username:
+       typed_username = "@{}".format(typed_username)
+    if ' ' in typed_username:
+        typed_username = typed_username.replace(' ', '_')
+    return typed_username
+
 def error_callback(bot, update, error):
     logging.error(error)
 
@@ -69,7 +82,6 @@ def main():
     dp.add_error_handler(error_callback)
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
-    #dp.add_handler(CommandHandler('phrase', phrase_generator))
     dp.add_handler(InlineQueryHandler(inlinequery))
 
     updater.start_polling()
